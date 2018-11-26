837ca4628299a3a56525769de01db57b84c90795
ShambhaviRB <shambhavibelligatti@gmail.com>
2018-11-26 18:48:41 +0530

    add GET /todos route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -11,18 +11,26 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
     var todo = new Todo({
-        text : req.body.text
+        text: req.body.text
     });
-    todo.save().then((doc) =>{
+    todo.save().then((doc) => {
         res.send(doc);
     },
-    (e) => {
-        res.status(400).send(e);
-    });
+        (e) => {
+            res.status(400).send(e);
+        });
 });
 
-app.listen(3000, ()=>{
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (err) => {
+        res.send(err);
+    })
+})
+
+app.listen(3000, () => {
     console.log("server is running in 3000")
 });
 
-module.exports = {app}
+module.exports = { app }
