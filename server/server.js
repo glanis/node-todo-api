209e33f5804351df32f07cb3132ae4cb038fe3cb
ShambhaviRB <shambhavibelligatti@gmail.com>
2018-11-27 11:33:13 +0530

    added GET /todos:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var {ObjectID} = require("mongodb");
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -28,6 +29,19 @@ app.get('/todos', (req, res) => {
         res.send(err);
     })
 })
+
+app.get('/todos/:id', (req, res) => {
+        id = req.params.id;
+        if (!ObjectID.isValid(id))
+            return res.status(404).send("not a valid ID")
+        Todo.findById(id).then((todo) => {
+            if(!todo)
+                return res.status(404).send("id "+id+ " not found in database");
+             res.status(200).send({todo});
+        }, (err) => {
+            return res.status(400).send(err);
+        })    
+});
 
 app.listen(3000, () => {
     console.log("server is running in 3000")
